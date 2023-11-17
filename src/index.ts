a0094c1763f8ab77474edbde67cb6b305edc1527
Sumit <sk990005@outlook.com>
2023-11-17 23:54:31 +0530

    add projects api - GET, POST

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Express } from "express";
 import config from "config";
+import "express-async-errors";
+
 import connectToDb from "./startup/db";
+import routes from "./startup/routes";
 
 const PORT: string | number = config.get<string>("PORT") || 3000;
 const app: Express = express();
@@ -8,4 +11,5 @@ const app: Express = express();
 app.listen(PORT, () => {
 	console.log(`⚡️Listening on port ${PORT}`);
 	connectToDb();
+	routes(app);
 });
