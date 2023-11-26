42708857a9ebfde5c2d62d48448960928389f9a2
Sumit <sk990005@outlook.com>
2023-11-27 00:21:58 +0530

    add swagger docs for flags

--- src/routes/flags.ts
+++ src/routes/flags.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import auth from "../middlewares/auth";
+
 import { validateBody } from "../middlewares/validateBody";
 import { validateData } from "../models/Flag";
 import validateId from "../middlewares/validateId";
